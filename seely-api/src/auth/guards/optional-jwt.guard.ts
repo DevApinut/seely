7299ec5cea9b096985a52742e09db77acb47d297
DevApinut <apinut.dev@gmail.com>
2025-10-03 09:25:24 +0700

    change

--- seely-api/src/auth/guards/optional-jwt.guard.ts
+++ seely-api/src/auth/guards/optional-jwt.guard.ts
@@ -3,17 +3,13 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalJwtGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    // Add your custom authentication logic here
-    // for example, call super.logIn(request) to establish a session.
+  canActivate(context: ExecutionContext) {   
     return super.canActivate(context);
   }
 
-  handleRequest(err: any, user: any, info: any) {
-    // You can throw an exception based on either "info" or "err" arguments
-    // return user object if valid, or null if not authenticated
+  handleRequest(err: any, user: any, info: any) {   
     if (err || !user) {
-      return null; // ไม่ throw error แต่ return null แทน
+      return null; 
     }
     return user;
   }
